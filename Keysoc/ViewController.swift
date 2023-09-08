cea1aac1fd15e381eecf858ada1e0fea14a63620
Tsz Sang Wong <tsz-sang.wong@ibm.com>
2023-09-08 14:00:35 +0800

    add dummy tableview

--- Keysoc/ViewController.swift
+++ Keysoc/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath) as! customTableCell
+        
+        cell.label_title.text = "\(indexPath.row)"
+        
+        return cell
+        
+    }
 
+}
+
+
+
+class customTableCell: UITableViewCell {
+    
+    @IBOutlet weak var label_title: UILabel!
+    
 }
 
